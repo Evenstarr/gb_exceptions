672020d058a946e8e936a6c0e7f16246a9c295c6
Evenstar <alhe@yandex.ru>
2023-09-26 20:08:31 +0300

    A bit of

--- Exc2/Task2.java
+++ Exc2/Task2.java
@@ -15,13 +15,14 @@ public class Task2 {
         // Это задание специально сделано, чтобы исключения не использовались?
         // Все получаются Checked.
 
-        int d = 0;
+        int d = 1;
         double[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int index = 8;
 
-        if (d == 0) {
-            System.out.println("Мы не делим на ноль");
-        } else if (intArray.length < 9) {
-            System.out.println("Неподходящая длина массива");
+        if (index < 0 || index > intArray.length){
+            System.out.println("Индекс за пределами массива");
+        } else if (d == 0) {
+            System.out.println("Мы не делим на 0");
         } else {
             double catchedRes1 = intArray[9] / d;
             System.out.println(catchedRes1);
